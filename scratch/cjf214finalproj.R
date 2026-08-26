4cebe10caba148ece203c350c456a0286ae1eb89
cuvlin <calvin@fuweb.com>
2026-08-26 12:02:45 -0700

    function created Fixes Issue #1

--- scratch/cjf214finalproj.R
+++ scratch/cjf214finalproj.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+source("R/moving_average.R")
 
 # Loading in Data --------------------------------------------------------
 
@@ -14,6 +14,26 @@ BQ2_raw <- read_csv("data/lter_data/QuebradaCuenca2-Bisley.csv") |> # Q1
   select(colnames)
 BQ3_raw <- read_csv("data/lter_data/QuebradaCuenca3-Bisley.csv") |> # Q3
   select(colnames)
+
+
+# Use moving_average function
+
+PRM_smoothed <- moving_average(PRM_raw)
+BQ1_smoothed <- moving_average(BQ1_raw)
+BQ2_smoothed <- moving_average(BQ2_raw)
+BQ3_smoothed <- moving_average(BQ3_raw)
+
+lter_smoothed <- rbind(PRM_smoothed, BQ1_smoothed, BQ2_smoothed, BQ3_smoothed)
+
+lter_smoothed |>
+  ggplot(
+    mapping = aes(x = window_start, y = ca, fill = sample_id)
+  ) +
+  geom_line()
+
+
+# Scratch work -----------------------------------------------------------
+#### BELOW CODE IS FUNCTION THOUGHT PROCESS NOT TO RUN
 
 lter_cat <- rbind(PRM_raw, BQ1_raw, BQ2_raw, BQ3_raw)
 
@@ -34,33 +54,33 @@ for (i in 1:length(lter_smoothed$window_start)) {
   we <- lter_smoothed$window_start[i] + 63
   # for K
   lter_smoothed$k[i] <- mean(
-    lter_cat$K[lter_cat$sample_date >= ws & lter_cat$sample_date < we],
+    lter_cat$K[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we],
     na.rm = TRUE
   )
   # for mg
   lter_smoothed$mg[i] <- mean(
-    lter_cat$Mg[lter_cat$sample_date >= ws & lter_cat$sample_date < we],
+    lter_cat$Mg[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we],
     na.rm = TRUE
   )
   # for ca
   lter_smoothed$ca[i] <- mean(
-    lter_cat$Ca[lter_cat$sample_date >= ws & lter_cat$sample_date < we],
+    lter_cat$Ca[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we],
     na.rm = TRUE
   )
   # for nh4
   lter_smoothed$nh4_n[i] <- mean(
-    lter_cat$'NH4_N'[lter_cat$sample_date >= ws & lter_cat$sample_date < we],
+    lter_cat$'NH4_N'[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we],
     na.rm = TRUE
   )
   # for no3
   lter_smoothed$no3_n[i] <- mean(
-    lter_cat$'NO3-N'[lter_cat$sample_date >= ws & lter_cat$sample_date < we],
+    lter_cat$'NO3-N'[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we],
     na.rm = TRUE
   )
   # For sample ID
-  lter_smoothed$sample_id[i] <- first(
-    lter_cat$Sample_ID[lter_cat$sample_date >= ws & lter_cat$sample_date < we]
-  )
+  #lter_smoothed$sample_id[i] <- first(
+  #lter_cat$Sample_ID[lter_cat$Sample_Date >= ws & lter_cat$Sample_Date < we]
+  #)
 }
 
 lter_cat |>
@@ -81,4 +101,11 @@ test_smoothed_local <- tibble(
   nh4_n = NA,
   ca = NA,
   Sample_Id = NA
+)
+
+test1 <- lter_smoothed$window_start[1]
+test2 <- lter_smoothed$window_start[1] + 63
+mean(
+  lter_cat$K[lter_cat$Sample_Date >= test1 & lter_cat$Sample_Date < test2],
+  na.rm = TRUE
 )
